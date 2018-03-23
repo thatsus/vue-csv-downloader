4b206a34d33418894899e48e9dc7488b732228d5
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2018-03-23 11:44:08 -0400

    Begin with tests

---
+++ test/test.js
@@ -0,0 +1,52 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import assert from 'assert';
+import json2csv from 'json2csv';
+import CsvDownload from '../src/CsvDownload.vue';
+import Vue from 'vue';
+
+module.exports = function () {
+
+    let vm, theDownloader;
+
+    beforeEach('setup the Vue instance', function (done) {
+
+        vm = new Vue({
+            template: `
+                <csv-download 
+                    :fields="fields"
+                    :data="items"
+                    ref="theDownloader"
+                >
+                </csv-download>
+            `,
+            components: {
+                CsvDownload
+            },
+            data() {
+                return {
+                    items: [
+                        {name: 'Dave', objectiveQuality: 'Medium'},
+                        {name: 'Dan', objectiveQuality: 'High'},
+                    ],
+                    fields: [
+                        'name',
+                        'objectiveQuality',
+                    ],
+                };
+            },
+        });
+
+        vm.$mount();
+
+        theDownloader = vm.$refs.theDownloader;
+
+        Vue.waitTicks(3)
+            .then(done);
+    });
+
+    it('should have loaded', function () {
+        assert(theDownloader, "theDownloader didn't load")
+    });
+};
+
