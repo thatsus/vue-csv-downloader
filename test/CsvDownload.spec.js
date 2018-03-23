e832c554fa43d4f14893c8a68182de5167070919
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2018-03-23 13:51:45 -0400

    Tests are really doing their job now.

--- test/CsvDownload.spec.js
+++ test/CsvDownload.spec.js
@@ -49,14 +49,16 @@ describe('CsvDownload', function () {
     });
 
     it('should have generated a link', function () {
-        assert(theDownloader.$el.href, "data:text/csv,%22name%22%2C%22objectiveQuality%22%0A%22Dave%22%2C%22Medium%22%0A%22Dan%22%2C%22High%22");
+        assert.equal(theDownloader.$el.href, "data:text/csv,%22name%22%2C%22objectiveQuality%22%0A%22Dave%22%2C%22Medium%22%0A%22Dan%22%2C%22High%22");
+        assert.equal(theDownloader.$el.download, "export.csv");
     });
 
     it('should change when field list changes', function (done) {
         vm.fields = ['name', 'office'];
         vm.$nextTick()
             .then(() => {
-                assert(theDownloader.$el.href, "data:text/csv,%22name%22%2C%office%22%0A%22Dave%22%2C%22304%22%0A%22Dan%22%2C%22189%22");
+                assert.equal(theDownloader.$el.href, "data:text/csv,%22name%22%2C%22office%22%0A%22Dave%22%2C304%0A%22Dan%22%2C189");
+                assert.equal(theDownloader.$el.download, "export.csv");
             })
             .then(done, done);
     });
@@ -66,7 +68,69 @@ describe('CsvDownload', function () {
         vm.items[1].name = 'Daryl';
         vm.$nextTick()
             .then(() => {
-                assert(theDownloader.$el.href, "data:text/csv,%22name%22%2C%office%22%0A%22Randy%22%2C%22304%22%0A%22Daryl%22%2C%22189%22");
+                assert.equal(theDownloader.$el.href, "data:text/csv,%22name%22%2C%22objectiveQuality%22%0A%22Randy%22%2C%22Medium%22%0A%22Daryl%22%2C%22High%22");
+                assert.equal(theDownloader.$el.download, "export.csv");
+            })
+            .then(done, done);
+    });
+});
+
+describe('CsvDownload', function () {
+
+    let vm, theDownloader;
+
+    beforeEach('setup the Vue instance', function (done) {
+
+        vm = new Vue({
+            template: `
+                <csv-download 
+                    :fields="fields"
+                    :data="items"
+                    :download-name="downloadName"
+                    ref="theDownloader"
+                >
+                </csv-download>
+            `,
+            components: {
+                CsvDownload
+            },
+            data() {
+                return {
+                    items: [
+                        {spirit: 'Avarice', dangerFactor: 40},
+                        {spirit: 'Gluttony', dangerFactor: 70},
+                    ],
+                    fields: [
+                        'spirit',
+                        'dangerFactor',
+                    ],
+                    downloadName: 'somefile.csv',
+                };
+            },
+        });
+
+        vm.$mount();
+
+        theDownloader = vm.$refs.theDownloader;
+
+        Vue.waitTicks(3).then(done);
+    });
+
+    it('should have loaded', function () {
+        assert(theDownloader, "theDownloader didn't load")
+    });
+
+    it('should have generated a link with new filename', function () {
+        assert.equal(theDownloader.$el.href, "data:text/csv,%22spirit%22%2C%22dangerFactor%22%0A%22Avarice%22%2C40%0A%22Gluttony%22%2C70");
+        assert.equal(theDownloader.$el.download, "somefile.csv");
+    });
+
+    it('should change the filename', function (done) {
+        vm.downloadName = 'someotherfile.csv';
+        vm.$nextTick()
+            .then(() => {
+                assert.equal(theDownloader.$el.href, "data:text/csv,%22spirit%22%2C%22dangerFactor%22%0A%22Avarice%22%2C40%0A%22Gluttony%22%2C70");
+                assert.equal(theDownloader.$el.download, "someotherfile.csv");
             })
             .then(done, done);
     });
