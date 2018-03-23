86cb39599fe05276ba7c63dd121815ba353a9bff
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2018-03-23 14:13:33 -0400

    Gulp our file into dist

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+
+var elixir = require('./elixir.js');
+
+
+elixir.ready(function () {
+  elixir.webpack.mergeConfig({
+    output: {
+      libraryTarget: 'commonjs',
+    },
+  });
+});
+
+elixir(function (mix) {
+    mix.webpack('CsvDownload.vue', './dist', './src');
+});
